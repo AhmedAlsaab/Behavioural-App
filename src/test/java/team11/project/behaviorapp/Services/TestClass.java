cc7927507a2b1a73c5897f1d80f498b141df1869
Ahmed Alsaab <AlsaabA@cardiff.ac.uk>
2018-03-21 14:49:27 +0000

    Installed PMD - Further tests in test class

--- src/test/java/team11/project/behaviorapp/Services/TestClass.java
+++ src/test/java/team11/project/behaviorapp/Services/TestClass.java
@@ -7,6 +7,12 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.TriggerContext;
+import org.springframework.scheduling.support.CronTrigger;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class TestClass {
     protected Logger logger = LoggerFactory.getLogger(this.getClass());
@@ -20,6 +26,47 @@ public class TestClass {
         assertEquals("onetwo", result);
 
         String message = "The result is : " + result;
+        logger.info(message);
+
+    }
+
+    @Test
+    public void cronTest(){
+        // import the cron scheduler
+        org.springframework.scheduling.support.CronTrigger trigger =
+                // Setting it to run at every minute of every hour of everyday ....
+                new CronTrigger("0 0/1 * * * *");
+
+        // Use the calendar to set the days
+        Calendar today = Calendar.getInstance();
+        today.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY);
+
+        SimpleDateFormat df = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss EEEE");
+
+        final Date yesterday = today.getTime();
+
+        logger.info("Last Execution : " + df.format(yesterday));
+
+        Date nextExecutionTime = trigger.nextExecutionTime(
+                new TriggerContext() {
+
+                    @Override
+                    public Date lastScheduledExecutionTime() {
+                        return yesterday;
+                    }
+
+                    @Override
+                    public Date lastActualExecutionTime() {
+                        return yesterday;
+                    }
+
+                    @Override
+                    public Date lastCompletionTime() {
+                        return yesterday;
+                    }
+                });
+
+        String message = "Next Execution : " + df.format(nextExecutionTime);
         logger.info(message);
 
     }
